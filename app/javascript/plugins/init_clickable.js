461bc24029816cc5084a9eac94b0d590f313ad0d
Mark Prospero <mark_lee31@hotmail.com>
2020-12-02 22:13:53 +0100

    adicionei javascript

--- app/javascript/plugins/init_clickable.js
+++ app/javascript/plugins/init_clickable.js
@@ -14,9 +14,25 @@ sports.forEach(toggleActiveOnClick);
 
 };
 
-export { initClick };
+const clickableAmenity = () => {
+  const amenityBtn = document.querySelector('#btn-amenities')
+  if (amenityBtn) {
+    amenityBtn.addEventListener('click',( ) => {
+      const selectedAmenities = document.querySelectorAll(".clickable.active")
+      let relativePath = "/neighborhoods?amenities="
+      const amenities = []
+      selectedAmenities.forEach((amenity) => amenities.push(amenity.dataset.amenity))
+      window.location.replace(relativePath + amenities.join(","))
+    });
+  }
+};
+
+export { initClick, clickableAmenity };
+
 
 // pegar todos os elementos que com a classe active(javascript)
 // por meio do data-attribute mandar para a action da controller
 // form tag rails
 // params
+
+
