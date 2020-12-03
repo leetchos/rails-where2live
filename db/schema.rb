d17ec4472d4104bc69e4f3a253de702e382e61f5
Eros Souza <eros.magno@yahoo.com.br>
2020-12-03 18:22:56 -0300

    profiletest

--- db/schema.rb
+++ db/schema.rb
@@ -37,7 +37,12 @@ ActiveRecord::Schema.define(version: 2020_12_03_153157) do
   end
 
   create_table "amenities", force: :cascade do |t|
-    t.string "name"
+    t.integer "gym"
+    t.integer "school"
+    t.integer "supermarket"
+    t.integer "hospital"
+    t.integer "restaurant"
+    t.integer "mobility"
     t.datetime "created_at", precision: 6, null: false
     t.datetime "updated_at", precision: 6, null: false
   end
