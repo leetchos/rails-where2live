a26ab1875e74d3602d2c1ccedd9deaa66f0fa98c
Mark Prospero <mark_lee31@hotmail.com>
2020-12-03 18:56:41 +0100

    seed feito e rota e view de review feito

--- app/controllers/reviews_controller.rb
+++ app/controllers/reviews_controller.rb
@@ -1,4 +1,25 @@
 class ReviewsController < ApplicationController
   def show
   end
+
+  class ReviewsController < ApplicationController
+  def create
+    @neighborhood = Neighborhood.find(params[:neighborhood_id])
+    @review = Review.new(review_params)
+    @review.neighborhood = @neighborhood
+    @review.user = current_user
+    if @review.save
+      redirect_to neighborhood_path(@neighborhood)
+    else
+      render "neighborhoods/show"
+    end
+  end
+
+  private
+
+  def review_params
+    params.require(:review).permit(:rating, :content)
+  end
+end
+
 end
