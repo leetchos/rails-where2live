037121cb486108ccaca87e3db08797ccd4efb781
raphael costa <raphaeljsc@gmail.com>
2020-12-07 13:11:48 -0300

    ActionCable - chat

--- config/routes.rb
+++ config/routes.rb
@@ -7,6 +7,7 @@ Rails.application.routes.draw do
   resources :users, only: [:index, :edit]
   resources :amenities, only: [:index]
   resources :cities, only: [:index]
+  resources :chatrooms, only: [:show]
 
   # For details on the DSL available within this file, see https://guides.rubyonrails.org/routing.html
 end
